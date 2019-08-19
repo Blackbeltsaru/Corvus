58d279912a512c8ea496ce7a2b3939d56cbfab35
Blackbeltsaru <s-rbiggs@hotmail.com>
2019-08-19 17:44:04 -0500

    shader debugging

--- distribution/Corvus/Core/Application.js
+++ distribution/Corvus/Core/Application.js
@@ -48,180 +48,167 @@ function BIT(x) {
     return 1 << x;
 };
 
+// class Application {
+
+//     //Class variables 
+//     // _Window;
+//     // _Running;
+//     // _LayerStack;
+
+//     //vertextArray
+//     //vertexBuffer
+//     //indexBuffer
+
+//     // static s_Instance;
+//     static getInstance() {
+//         return Application.s_instance;
+//     }
+
+//     /**
+//      * This initialized the application.
+//      * Create the window and sets the eventCallback for the window
+//      */
+//     constructor() {
+//         //TODO: logging should be removed from release builds
+//         CorvusLogger.coreLogger.info('Constructing Application');
+//         CorvusLogger.coreLogger.assert(!Application.getInstance(), "Application already exists");
+//         Application.s_Instance = this;
+
+//         //Bind functions
+//         this.onEvent = this.onEvent.bind(this);
+//         this.run = this.run.bind(this);
+
+//         this._Running = true;
+//         this._Window = WebWindow.create(new WindowProps());
+//         this._Window.setEventCallback(this.onEvent);
+//         this._LayerStack = new LayerStack();
+//         CorvusLogger.coreLogger.info('Application constructed with ', this._Running, this._Window, this._LayerStack);
+
+
+//         //Setup webGL buffers
+//         //HACK
+//         //=================================================================================
+//         //=================================================================================
+//         //TODO:(Ryan) this is webGL specific and should be move to a platform file
+//         let context = this._Window.getContext().getGraphicsContext();
+//         //TODO:(Ryan) read about these methods and understand whats going on
+
+//         CorvusLogger.GetCoreLogger().warn("Finished Rendering");
+
+
+//         //=================================================================================
+//         //=================================================================================
+//         //END HACK
+
+//         //Bind functions
+//     }
+
+//     /**
+//      * The event callback used by the window
+//      * @param {Event} event The event that will be propagated 
+//      */
+//     onEvent(event) {
+//         let dispatcher = new EventDispatcher(event);
+
+//         for(let it = this._LayerStack.end(); it !== this._LayerStack.begin(); it--) {
+//             this._LayerStack.get(it).onEvent(e);
+//             if(e.handled) break;
+//         }
+//     }
+
+//     /**
+//      * The main run loop
+//      */
+//     run() {
+//         //TODO: do application update-y stuff here
+//         // let context = this._Window.getContext().getGraphicsContext();
+
+//         //context.clearColor(0.8, 0.2, 0.3, 0.9);
+//         let canvas = document.getElementById('canvas');
+//         let context = canvas.getContext('webgl2');
+//         context.enable(context.DEPTH_TEST);
+//         context.clear(context.COLOR_BUFFER_BIT);
+//         context.clear(context.DEPTH_BUFFER_BIT);
+//         context.viewport(0, 0, 1280, 720);
+
+//         // this.vertextArray = context.createVertexArray();
+//         // context.bindVertexArray(this.vertextArray);
+//         let verticies = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5];
+
+//         this.vertextBuffer = context.createBuffer();
+//         context.bindBuffer(context.ARRAY_BUFFER, this.vertextBuffer);
+//         context.bufferData(context.ARRAY_BUFFER, new Float32Array(verticies), context.STATIC_DRAW);
+//         context.bindBuffer(context.ARRAY_BUFFER, null);
+
+//         let vertexSrc = 
+//         'attribute vec2 coords;' +
+//         'void main(void) {' +
+//         ' gl_Position = vec4(coords, 0.0, 1.0);' + 
+//         '}';
+//         let vertShader = _compileShader(context, context.VERTEX_SHADER, vertexSrc);
+
+//         let fragmentSrc = 
+//         'void main(void) {' +
+//         ' gl_FragColor = vec4(0.2, 0.8, 0.3, 0.1);' + 
+//         '}';
+//         let fragShader = _compileShader(context, context.FRAGMENT_SHADER, fragmentSrc);
+//         let shaderProgram = _programShader(context, vertShader, fragShader);
+
+//         // this.shader = new Shader(context, vertexSrc, fragmentSrc);
+
+//         context.bindBuffer(context.ARRAY_BUFFER, this.vertextBuffer)
+//         let coords = context.getAttribLocation(shaderProgram, "coords");
+//         context.vertexAttribPointer(coords, 2, context.FLOAT, false, 0, 0);
+//         context.enableVertexAttribArray(coords);
+
+//         // this.indexBuffer = context.createBuffer();
+//         // context.bindBuffer(context.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+//         // let indices = [0, 1, 2];
+//         // context.bufferData(context.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), context.STATIC_DRAW);
+
+//         // this.shader.bind()
+//         context.drawArrays(context.TRIANGLES, 0, verticies.length);
+
+//         // for(let it = this._LayerStack.begin(); it !== this._LayerStack.end(); it++) {
+//         //     this._LayerStack.get(it).onUpdate();
+//         // }
+//         // //TODO:(Ryan) Do we need to have a layer render here?
+
+//         // if(this._Running) this._Window.onUpdate(this.run);
+//     }
+
+//     /** @returns {Window} the current window of the application */
+//     getWindow() {return this._Window;}
+//     /** @returns {Application} the current instance of the application */
+//     static get() {return Application.getInstance();}
+
+//     /**
+//      * A static method to create the application
+//      * This should be implemented by the client
+//      */
+//     static createApplication() {
+//         throw new NotImplementedError();
+//     }
+
+//     pushLayer(layer) {
+//         this._LayerStack.pushLayer(layer);
+//         layer.onAttach();
+//     }
+
+//     pushOverlay(layer) {
+//         this._LayerStack.pushOverlay(layer);
+//     }
+// }
+
 var Application = function () {
-    _createClass(Application, null, [{
-        key: 'getInstance',
-
-
-        //Class variables 
-        // _Window;
-        // _Running;
-        // _LayerStack;
-
-        //vertextArray
-        //vertexBuffer
-        //indexBuffer
-
-        // static s_Instance;
-        value: function getInstance() {
-            return Application.s_instance;
-        }
-
-        /**
-         * This initialized the application.
-         * Create the window and sets the eventCallback for the window
-         */
-
-    }]);
-
     function Application() {
         _classCallCheck(this, Application);
     }
-    // //TODO: logging should be removed from release builds
-    // CorvusLogger.coreLogger.info('Constructing Application');
-    // CorvusLogger.coreLogger.assert(!Application.getInstance(), "Application already exists");
-    // Application.s_Instance = this;
-
-    // //Bind functions
-    // this.onEvent = this.onEvent.bind(this);
-    // this.run = this.run.bind(this);
-
-    // this._Running = true;
-    // this._Window = WebWindow.create(new WindowProps());
-    // this._Window.setEventCallback(this.onEvent);
-    // this._LayerStack = new LayerStack();
-    // CorvusLogger.coreLogger.info('Application constructed with ', this._Running, this._Window, this._LayerStack);
-
-
-    // //Setup webGL buffers
-    // //HACK
-    // //=================================================================================
-    // //=================================================================================
-    // //TODO:(Ryan) this is webGL specific and should be move to a platform file
-    // let context = this._Window.getContext().getGraphicsContext();
-    // //TODO:(Ryan) read about these methods and understand whats going on
-
-    // CorvusLogger.GetCoreLogger().warn("Finished Rendering");
-
-
-    // //=================================================================================
-    // //=================================================================================
-    // //END HACK
-
-    // //Bind functions
-
-
-    /**
-     * The event callback used by the window
-     * @param {Event} event The event that will be propagated 
-     */
-
 
     _createClass(Application, [{
-        key: 'onEvent',
-        value: function onEvent(event) {
-            var dispatcher = new _Events.EventDispatcher(event);
-
-            for (var it = this._LayerStack.end(); it !== this._LayerStack.begin(); it--) {
-                this._LayerStack.get(it).onEvent(e);
-                if (e.handled) break;
-            }
-        }
-
-        /**
-         * The main run loop
-         */
-
-    }, {
         key: 'run',
-        value: function run() {
-            //TODO: do application update-y stuff here
-            // let context = this._Window.getContext().getGraphicsContext();
-
-            //context.clearColor(0.8, 0.2, 0.3, 0.9);
-            var canvas = document.getElementById('canvas');
-            var context = canvas.getContext('webgl2');
-            context.enable(context.DEPTH_TEST);
-            context.clear(context.COLOR_BUFFER_BIT);
-            context.clear(context.DEPTH_BUFFER_BIT);
-            context.viewport(0, 0, 1280, 720);
-
-            // this.vertextArray = context.createVertexArray();
-            // context.bindVertexArray(this.vertextArray);
-            var verticies = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5];
-
-            this.vertextBuffer = context.createBuffer();
-            context.bindBuffer(context.ARRAY_BUFFER, this.vertextBuffer);
-            context.bufferData(context.ARRAY_BUFFER, new Float32Array(verticies), context.STATIC_DRAW);
-            context.bindBuffer(context.ARRAY_BUFFER, null);
-
-            var vertexSrc = 'attribute vec2 coords;' + 'void main(void) {' + ' gl_Position = vec4(coords, 0.0, 1.0);' + '}';
-            var vertShader = _compileShader(context, context.VERTEX_SHADER, vertexSrc);
-
-            var fragmentSrc = 'void main(void) {' + ' gl_FragColor = vec4(0.2, 0.8, 0.3, 0.1);' + '}';
-            var fragShader = _compileShader(context, context.FRAGMENT_SHADER, fragmentSrc);
-            var shaderProgram = _programShader(context, vertShader, fragShader);
-
-            // this.shader = new Shader(context, vertexSrc, fragmentSrc);
-
-            context.bindBuffer(context.ARRAY_BUFFER, this.vertextBuffer);
-            var coords = context.getAttribLocation(shaderProgram, "coords");
-            context.vertexAttribPointer(coords, 2, context.FLOAT, false, 0, 0);
-            context.enableVertexAttribArray(coords);
-
-            // this.indexBuffer = context.createBuffer();
-            // context.bindBuffer(context.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-
-            // let indices = [0, 1, 2];
-            // context.bufferData(context.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), context.STATIC_DRAW);
-
-            // this.shader.bind()
-            context.drawArrays(context.TRIANGLES, 0, verticies.length);
-
-            // for(let it = this._LayerStack.begin(); it !== this._LayerStack.end(); it++) {
-            //     this._LayerStack.get(it).onUpdate();
-            // }
-            // //TODO:(Ryan) Do we need to have a layer render here?
-
-            // if(this._Running) this._Window.onUpdate(this.run);
-        }
-
-        /** @returns {Window} the current window of the application */
-
-    }, {
-        key: 'getWindow',
-        value: function getWindow() {
-            return this._Window;
-        }
-        /** @returns {Application} the current instance of the application */
-
-    }, {
-        key: 'pushLayer',
-        value: function pushLayer(layer) {
-            this._LayerStack.pushLayer(layer);
-            layer.onAttach();
-        }
-    }, {
-        key: 'pushOverlay',
-        value: function pushOverlay(layer) {
-            this._LayerStack.pushOverlay(layer);
-        }
-    }], [{
-        key: 'get',
-        value: function get() {
-            return Application.getInstance();
-        }
-
-        /**
-         * A static method to create the application
-         * This should be implemented by the client
-         */
-
-    }, {
-        key: 'createApplication',
-        value: function createApplication() {
-            throw new _NotImplementedError2.default();
-        }
+        value: function run() {}
     }]);
 
     return Application;
--- src/Corvus/Core/Application.js
+++ src/Corvus/Core/Application.js
@@ -12,156 +12,165 @@ import Shader from '../Shader/Shader'
 //This can be used for bitwise operations 
 export function BIT(x){return 1 << x}; 
 
-class Application {
+// class Application {
 
-    //Class variables 
-    // _Window;
-    // _Running;
-    // _LayerStack;
+//     //Class variables 
+//     // _Window;
+//     // _Running;
+//     // _LayerStack;
 
-    //vertextArray
-    //vertexBuffer
-    //indexBuffer
+//     //vertextArray
+//     //vertexBuffer
+//     //indexBuffer
 
-    // static s_Instance;
-    static getInstance() {
-        return Application.s_instance;
-    }
+//     // static s_Instance;
+//     static getInstance() {
+//         return Application.s_instance;
+//     }
 
-    /**
-     * This initialized the application.
-     * Create the window and sets the eventCallback for the window
-     */
-    constructor() {
-        // //TODO: logging should be removed from release builds
-        // CorvusLogger.coreLogger.info('Constructing Application');
-        // CorvusLogger.coreLogger.assert(!Application.getInstance(), "Application already exists");
-        // Application.s_Instance = this;
+//     /**
+//      * This initialized the application.
+//      * Create the window and sets the eventCallback for the window
+//      */
+//     constructor() {
+//         //TODO: logging should be removed from release builds
+//         CorvusLogger.coreLogger.info('Constructing Application');
+//         CorvusLogger.coreLogger.assert(!Application.getInstance(), "Application already exists");
+//         Application.s_Instance = this;
 
-        // //Bind functions
-        // this.onEvent = this.onEvent.bind(this);
-        // this.run = this.run.bind(this);
+//         //Bind functions
+//         this.onEvent = this.onEvent.bind(this);
+//         this.run = this.run.bind(this);
 
-        // this._Running = true;
-        // this._Window = WebWindow.create(new WindowProps());
-        // this._Window.setEventCallback(this.onEvent);
-        // this._LayerStack = new LayerStack();
-        // CorvusLogger.coreLogger.info('Application constructed with ', this._Running, this._Window, this._LayerStack);
-
-
-        // //Setup webGL buffers
-        // //HACK
-        // //=================================================================================
-        // //=================================================================================
-        // //TODO:(Ryan) this is webGL specific and should be move to a platform file
-        // let context = this._Window.getContext().getGraphicsContext();
-        // //TODO:(Ryan) read about these methods and understand whats going on
-
-        // CorvusLogger.GetCoreLogger().warn("Finished Rendering");
+//         this._Running = true;
+//         this._Window = WebWindow.create(new WindowProps());
+//         this._Window.setEventCallback(this.onEvent);
+//         this._LayerStack = new LayerStack();
+//         CorvusLogger.coreLogger.info('Application constructed with ', this._Running, this._Window, this._LayerStack);
 
 
-        // //=================================================================================
-        // //=================================================================================
-        // //END HACK
+//         //Setup webGL buffers
+//         //HACK
+//         //=================================================================================
+//         //=================================================================================
+//         //TODO:(Ryan) this is webGL specific and should be move to a platform file
+//         let context = this._Window.getContext().getGraphicsContext();
+//         //TODO:(Ryan) read about these methods and understand whats going on
 
-        // //Bind functions
-    }
+//         CorvusLogger.GetCoreLogger().warn("Finished Rendering");
 
-    /**
-     * The event callback used by the window
-     * @param {Event} event The event that will be propagated 
-     */
-    onEvent(event) {
-        let dispatcher = new EventDispatcher(event);
 
-        for(let it = this._LayerStack.end(); it !== this._LayerStack.begin(); it--) {
-            this._LayerStack.get(it).onEvent(e);
-            if(e.handled) break;
-        }
-    }
+//         //=================================================================================
+//         //=================================================================================
+//         //END HACK
 
-    /**
-     * The main run loop
-     */
-    run() {
-        //TODO: do application update-y stuff here
-        // let context = this._Window.getContext().getGraphicsContext();
+//         //Bind functions
+//     }
 
-        //context.clearColor(0.8, 0.2, 0.3, 0.9);
-        let canvas = document.getElementById('canvas');
-        let context = canvas.getContext('webgl2');
-        context.enable(context.DEPTH_TEST);
-        context.clear(context.COLOR_BUFFER_BIT);
-        context.clear(context.DEPTH_BUFFER_BIT);
-        context.viewport(0, 0, 1280, 720);
+//     /**
+//      * The event callback used by the window
+//      * @param {Event} event The event that will be propagated 
+//      */
+//     onEvent(event) {
+//         let dispatcher = new EventDispatcher(event);
 
-        // this.vertextArray = context.createVertexArray();
-        // context.bindVertexArray(this.vertextArray);
-        let verticies = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5];
+//         for(let it = this._LayerStack.end(); it !== this._LayerStack.begin(); it--) {
+//             this._LayerStack.get(it).onEvent(e);
+//             if(e.handled) break;
+//         }
+//     }
 
-        this.vertextBuffer = context.createBuffer();
-        context.bindBuffer(context.ARRAY_BUFFER, this.vertextBuffer);
-        context.bufferData(context.ARRAY_BUFFER, new Float32Array(verticies), context.STATIC_DRAW);
-        context.bindBuffer(context.ARRAY_BUFFER, null);
+//     /**
+//      * The main run loop
+//      */
+//     run() {
+//         //TODO: do application update-y stuff here
+//         // let context = this._Window.getContext().getGraphicsContext();
 
-        let vertexSrc = 
-        'attribute vec2 coords;' +
-        'void main(void) {' +
-        ' gl_Position = vec4(coords, 0.0, 1.0);' + 
-        '}';
-        let vertShader = _compileShader(context, context.VERTEX_SHADER, vertexSrc);
+//         //context.clearColor(0.8, 0.2, 0.3, 0.9);
+//         let canvas = document.getElementById('canvas');
+//         let context = canvas.getContext('webgl2');
+//         context.enable(context.DEPTH_TEST);
+//         context.clear(context.COLOR_BUFFER_BIT);
+//         context.clear(context.DEPTH_BUFFER_BIT);
+//         context.viewport(0, 0, 1280, 720);
 
-        let fragmentSrc = 
-        'void main(void) {' +
-        ' gl_FragColor = vec4(0.2, 0.8, 0.3, 0.1);' + 
-        '}';
-        let fragShader = _compileShader(context, context.FRAGMENT_SHADER, fragmentSrc);
-        let shaderProgram = _programShader(context, vertShader, fragShader);
+//         // this.vertextArray = context.createVertexArray();
+//         // context.bindVertexArray(this.vertextArray);
+//         let verticies = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5];
 
-        // this.shader = new Shader(context, vertexSrc, fragmentSrc);
+//         this.vertextBuffer = context.createBuffer();
+//         context.bindBuffer(context.ARRAY_BUFFER, this.vertextBuffer);
+//         context.bufferData(context.ARRAY_BUFFER, new Float32Array(verticies), context.STATIC_DRAW);
+//         context.bindBuffer(context.ARRAY_BUFFER, null);
 
-        context.bindBuffer(context.ARRAY_BUFFER, this.vertextBuffer)
-        let coords = context.getAttribLocation(shaderProgram, "coords");
-        context.vertexAttribPointer(coords, 2, context.FLOAT, false, 0, 0);
-        context.enableVertexAttribArray(coords);
+//         let vertexSrc = 
+//         'attribute vec2 coords;' +
+//         'void main(void) {' +
+//         ' gl_Position = vec4(coords, 0.0, 1.0);' + 
+//         '}';
+//         let vertShader = _compileShader(context, context.VERTEX_SHADER, vertexSrc);
 
-        // this.indexBuffer = context.createBuffer();
-        // context.bindBuffer(context.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+//         let fragmentSrc = 
+//         'void main(void) {' +
+//         ' gl_FragColor = vec4(0.2, 0.8, 0.3, 0.1);' + 
+//         '}';
+//         let fragShader = _compileShader(context, context.FRAGMENT_SHADER, fragmentSrc);
+//         let shaderProgram = _programShader(context, vertShader, fragShader);
 
-        // let indices = [0, 1, 2];
-        // context.bufferData(context.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), context.STATIC_DRAW);
+//         // this.shader = new Shader(context, vertexSrc, fragmentSrc);
 
-        // this.shader.bind()
-        context.drawArrays(context.TRIANGLES, 0, verticies.length);
+//         context.bindBuffer(context.ARRAY_BUFFER, this.vertextBuffer)
+//         let coords = context.getAttribLocation(shaderProgram, "coords");
+//         context.vertexAttribPointer(coords, 2, context.FLOAT, false, 0, 0);
+//         context.enableVertexAttribArray(coords);
+
+//         // this.indexBuffer = context.createBuffer();
+//         // context.bindBuffer(context.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+//         // let indices = [0, 1, 2];
+//         // context.bufferData(context.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), context.STATIC_DRAW);
+
+//         // this.shader.bind()
+//         context.drawArrays(context.TRIANGLES, 0, verticies.length);
         
-        // for(let it = this._LayerStack.begin(); it !== this._LayerStack.end(); it++) {
-        //     this._LayerStack.get(it).onUpdate();
-        // }
-        // //TODO:(Ryan) Do we need to have a layer render here?
+//         // for(let it = this._LayerStack.begin(); it !== this._LayerStack.end(); it++) {
+//         //     this._LayerStack.get(it).onUpdate();
+//         // }
+//         // //TODO:(Ryan) Do we need to have a layer render here?
 
-        // if(this._Running) this._Window.onUpdate(this.run);
-    }
+//         // if(this._Running) this._Window.onUpdate(this.run);
+//     }
 
-    /** @returns {Window} the current window of the application */
-    getWindow() {return this._Window;}
-    /** @returns {Application} the current instance of the application */
-    static get() {return Application.getInstance();}
+//     /** @returns {Window} the current window of the application */
+//     getWindow() {return this._Window;}
+//     /** @returns {Application} the current instance of the application */
+//     static get() {return Application.getInstance();}
 
-    /**
-     * A static method to create the application
-     * This should be implemented by the client
-     */
-    static createApplication() {
-        throw new NotImplementedError();
-    }
+//     /**
+//      * A static method to create the application
+//      * This should be implemented by the client
+//      */
+//     static createApplication() {
+//         throw new NotImplementedError();
+//     }
 
-    pushLayer(layer) {
-        this._LayerStack.pushLayer(layer);
-        layer.onAttach();
-    }
+//     pushLayer(layer) {
+//         this._LayerStack.pushLayer(layer);
+//         layer.onAttach();
+//     }
     
-    pushOverlay(layer) {
-        this._LayerStack.pushOverlay(layer);
+//     pushOverlay(layer) {
+//         this._LayerStack.pushOverlay(layer);
+//     }
+// }
+
+class Application {
+    constructor() {
+
+    }
+    run() {
+        
     }
 }
 
